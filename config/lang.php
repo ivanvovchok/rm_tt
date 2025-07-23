a33cbb20daa087df27fcded366b4e725f3e66218
Ivan Vovchok <elvovchok@gmail.com>
2025-07-24 02:10:04 +0300

    Add database config and lang file

---
+++ config/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'home_title'     => 'Home',
+    'login_title'    => 'Login',
+    'register_title' => 'Register',
+
+    'invalid_email'       => 'Invalid email',
+    'invalid_password'    => 'Password must be at least 8 characters',
+    'invalid_phone'       => 'Invalid phone number',
+    'invalid_credentials' => 'Invalid credentials',
+    'email_exists'        => 'This email is already registered.',
+
+    'first_name_required' => 'First name is required',
+    'last_name_required'  => 'Last name is required',
+];
